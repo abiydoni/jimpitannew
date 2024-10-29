b1026ec4e973016c8d5a6200dc3681645b80b44f
abiydoni <abiydoni@gmail.com>
2024-10-29 18:59:11 +0700

    Update report.php

--- dashboard/report.php
+++ dashboard/report.php
@@ -33,9 +33,6 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <!-- Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
