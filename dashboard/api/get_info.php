d52e7a9fe41526aa85f7e2bafce6f7827ad677a0
abiydoni <abiydoni@gmail.com>
2024-11-01 05:42:51 +0700

    info

--- dashboard/api/get_info.php
+++ dashboard/api/get_info.php
@@ -1,14 +1,19 @@
 <?php
 include 'db.php';
-// Query untuk menghitung jumlah data
-$sqlkk = "SELECT COUNT(*) AS total_rows FROM master_kk";
-$resultkk = $pdo->query($sqlkk);
 
-// Mengambil jumlah data
-if ($resultkk->num_rows > 0) {
-    $rowkk = $resultkk->fetch_assoc();
-    echo json_encode(["total" => $rowkk["total_rows"]]);
+// Query untuk menghitung jumlah data
+$sql = "SELECT COUNT(*) AS total_rows FROM master_kk";
+$result = $pdo->query($sql);
+
+// Mengambil jumlah data dan mengirimkan dalam format JSON
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo json_encode(["total" => $row["total_rows"]]);
 } else {
     echo json_encode(["total" => 0]);
 }
+
+// Menutup koneksi
+$conn->close();
+
 ?>
