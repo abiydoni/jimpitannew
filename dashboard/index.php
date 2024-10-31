e4db295924f3e22a85332f73f87b011978af129c
abiydoni <abiydoni@gmail.com>
2024-10-31 18:40:36 +0700

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -108,13 +108,24 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </div>
             </div>
             
+            <?php
+            $query1 = "SELECT COUNT(*) AS jumlah_data FROM master_kk";
+            $result1 = $pdo->query($query1);
+
+            if ($result1->num_rows > 0) {
+                $row1 = $result1->fetch_assoc();
+                echo "<p>Jumlah data di tabel kas_umum: " . $row1['jumlah_data'] . "</p>";
+            } else {
+                echo "<p>Tidak ada data ditemukan.</p>";
+            }
+            ?>
 
             <ul class="box-info">
                 <li>
                     <i class='bx bxs-group bx-lg' ></i>
                     <span class="text">
                         <h3 id="totalPeserta">0</h3>
-                        <p>Total KK</p>
+                        <p><?php . $row1['jumlah_data'] . ?></p>
                     </span>
                 </li>
                 <li>
