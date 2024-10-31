6fb4601dcb68b96170e9f94430507629fb0cbc03
abiydoni <abiydoni@gmail.com>
2024-10-31 17:41:23 +0700

    Update grafik.js

--- dashboard/js/grafik.js
+++ dashboard/js/grafik.js
@@ -1,9 +1,9 @@
 // Data untuk grafik
 const data = {
-    labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni'],
+    labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'Nopember', 'Desember'],
     datasets: [{
         label: 'Jumlah Penjualan',
-        data: [65, 59, 80, 81, 56, 55],
+        data: [65, 59, 80, 81, 56, 55, 65, 59, 80, 81, 56, 55],
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 1
