56426e1725c9780cfda84ba43e8fce0679584860
abiydoni <abiydoni@gmail.com>
2024-11-03 01:02:08 +0700

    Update jadwal.php

--- dashboard/jadwal.php
+++ dashboard/jadwal.php
@@ -116,7 +116,7 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         </div>
                     </div>
                     <table id="example" class="display w-full text-left shadow-lg border border-gray-300" style="width:100%">
-                        <thead>
+                        <thead class="bg-gray-200">
                             <tr>
                                 <th style="text-align: left;">Kode ID</th>
                                 <th style="text-align: center;">Nama</th>
@@ -129,10 +129,10 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             if ($data) {
                                 foreach ($data as $row): ?>
                                     <tr>
-                                        <td class="border border-gray-300 px-4 py-2"><?php echo htmlspecialchars($row["id_code"]); ?></td>
-                                        <td class="border border-gray-300 px-4 py-2"><?php echo htmlspecialchars($row["name"]); ?></td>
-                                        <td class="border border-gray-300 px-4 py-2"><?php echo htmlspecialchars($row["shift"]); ?></td>
-                                        <td class="border border-gray-300 px-4 py-2" style="text-align: center;">
+                                        <td><?php echo htmlspecialchars($row["id_code"]); ?></td>
+                                        <td><?php echo htmlspecialchars($row["name"]); ?></td>
+                                        <td><?php echo htmlspecialchars($row["shift"]); ?></td>
+                                        <td style="text-align: center;">
                                             <button class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-3 rounded" onclick="openModal(<?= $row['id_code'] ?>, '<?= $row['user_name'] ?>')">Edit</button>
                                             <button onclick="deleteData(<?= $row['id_code'] ?>)" class="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded">
                                                 Hapus
