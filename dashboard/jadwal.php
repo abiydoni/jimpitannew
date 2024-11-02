4ae84192004e198db852610e885a8383a9bc576d
abiydoni <abiydoni@gmail.com>
2024-11-03 03:14:38 +0700

    Update jadwal.php

--- dashboard/jadwal.php
+++ dashboard/jadwal.php
@@ -208,7 +208,6 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     <script src="js/script.js"></script>
     <script src="js/print.js"></script>
-    <script src="js/crud.js"></script>
 	<script src="js/qrcode.min.js"></script>
 
 
@@ -260,6 +259,29 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 });
             });
         </script>
+    <script>
+        function editUser(id, idCode, userName, name, shift, role) {
+            document.getElementById('userId').value = id;
+            document.getElementById('idCode').value = idCode;
+            document.getElementById('userName').value = userName;
+            document.getElementById('name').value = name;
+            document.getElementById('password').value = ""; // Kosongkan password saat edit
+            document.getElementById('shift').value = shift;
+            document.getElementById('role').value = role;
+            document.getElementById('formTitle').innerText = "Edit Pengguna";
+        }
+
+        function cancelEdit() {
+            document.getElementById('userId').value = "";
+            document.getElementById('idCode').value = "";
+            document.getElementById('userName').value = "";
+            document.getElementById('name').value = "";
+            document.getElementById('password').value = "";
+            document.getElementById('shift').value = "";
+            document.getElementById('role').value = "";
+            document.getElementById('formTitle').innerText = "Tambah Pengguna";
+        }
+    </script>
 </body>
 </html>
 
