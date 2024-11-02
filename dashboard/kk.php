711735a41446d43464b5248bb405830822593272
abiydoni <abiydoni@gmail.com>
2024-11-03 01:52:07 +0700

    bootstrap

--- dashboard/kk.php
+++ dashboard/kk.php
@@ -149,6 +149,9 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <!-- MAIN -->
     </section>
     <!-- CONTENT -->    
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.tailwindcss.js"></script>
