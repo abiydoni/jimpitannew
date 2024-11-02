99267ceb8d7956c5fd92dd79c5661c4e4e8410fe
abiydoni <abiydoni@gmail.com>
2024-11-03 01:49:10 +0700

    Update jadwal.php

--- dashboard/jadwal.php
+++ dashboard/jadwal.php
@@ -188,8 +188,6 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </div>
     </div>
    
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
