4dfc8ba9fe9664b8a1a43d11fd43850e0a0e5644
abiydoni <abiydoni@gmail.com>
2024-11-06 16:46:43 +0700

    Update kk.php

--- dashboard/kk.php
+++ dashboard/kk.php
@@ -116,9 +116,7 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             <tr>
                                 <th class="border px-4 py-2">Code ID</th>
                                 <th class="border px-4 py-2">Nama KK</th>
-                                <th class="border px-4 py-2">Alamat</th>
                                 <th class="border px-4 py-2">HP</th>
-                                <th class="border px-4 py-2">Foto</th>
                                 <th style="text-align: center;">
                                     <input type="checkbox" id="selectAllCheckbox" style="display:none">
                                     <label for="selectAllCheckbox" style="font-size:24px"><i class='bx bx-check-double'></i></label>
@@ -136,9 +134,7 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                                 </a>
                                             </td>
                                             <td><?php echo htmlspecialchars($row["code_id"]); ?></td>
-                                            <td><?php echo htmlspecialchars($row["kk_alamat"]); ?></td>
                                             <td><?php echo htmlspecialchars($row["kk_hp"]); ?></td>
-                                            <td class="border px-4 py-2"><img src="uploads/<?php echo $row['kk_foto']; ?>" width="50"></td>
                                             <td class="border px-4 py-2">
                                                 <button class="bg-yellow-500 text-white px-2 py-1 rounded" data-modal-toggle="editModal" data-id="<?php echo $row['code_id']; ?>" data-name="<?php echo $row['kk_name']; ?>" data-alamat="<?php echo $row['kk_alamat']; ?>" data-hp="<?php echo $row['kk_hp']; ?>" data-foto="<?php echo $row['kk_foto']; ?>">Edit</button>
                                                 <a href="delete.php?id=<?php echo $row['code_id']; ?>" class="bg-red-500 text-white px-2 py-1 rounded">Hapus</a>
