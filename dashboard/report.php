8978541d575bc530cc70c1dc6d49bd25c5f7b69d
abiydoni <abiydoni@gmail.com>
2024-11-07 18:21:26 +0700

    Update report.php

--- dashboard/report.php
+++ dashboard/report.php
@@ -159,17 +159,18 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     </div>
                 </div>
             </div>
+                    <!-- Modal untuk memilih bulan dan tahun -->
+            <div id="monthYearModal" class="modal hidden">
+                <div class="modal-content">
+                    <span class="close-button">&times;</span>
+                    <h2>Pilih Bulan dan Tahun</h2>
+                    <input type="text" id="monthPickerModal" name="month-year" class="custom-select" placeholder="Pilih Bulan & Tahun">
+                    <button type="button" id="confirmSelection" class="btn-confirm">Konfirmasi</button>
+                </div>
+            </div>
+
         </main>
 
-        <!-- Modal untuk memilih bulan dan tahun -->
-        <div id="monthYearModal" class="modal hidden">
-            <div class="modal-content">
-                <span class="close-button">&times;</span>
-                <h2>Pilih Bulan dan Tahun</h2>
-                <input type="text" id="monthPickerModal" name="month-year" class="custom-select" placeholder="Pilih Bulan & Tahun">
-                <button type="button" id="confirmSelection" class="btn-confirm">Konfirmasi</button>
-            </div>
-        </div>
 
         <!-- MAIN -->
     </section>
