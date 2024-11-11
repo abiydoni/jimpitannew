f9c9fac7f6c92699d81b0cc7fe7f246a2993f900
abiydoni <abiydoni@gmail.com>
2024-11-11 17:54:43 +0700

    Update jadwal.php

--- dashboard/jadwal.php
+++ dashboard/jadwal.php
@@ -147,8 +147,7 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                     <a href="jadwal.php?delete=<?php echo $user['id_code']; ?>" onclick="return confirm('Yakin ingin menghapus data <?php echo $user['name']; ?> ?')" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded">
                                         <i class='bx bx-trash'></i> <!-- Ikon hapus ditambahkan -->
                                     </a>
-                                    <button onclick="openChangePasswordModal('<?php echo $user['id_code']; ?>')" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-3 rounded">Ubah Password</button>
-                                    <button onclick="alert('<?php echo htmlspecialchars($user['password']); ?>')" class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded">Tampilkan Password</button>
+                                    <button onclick="openChangePasswordModal('<?php echo $user['id_code']; ?>', '<?php echo $user['name']; ?>')" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-3 rounded">Ubah Password</button>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
@@ -251,6 +250,8 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <form action="api/change_password.php" method="POST" class="space-y-2">
                 <input type="hidden" name="id_code" id="change_id_code">
                 <div class="bg-white p-2 rounded-lg shadow-md">
+                    <label class="block text-sm font-medium text-gray-700">Nama User:</label>
+                    <input type="text" name="name" id="change_user_name" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500" required readonly>
                     <label class="block text-sm font-medium text-gray-700">Password Baru:</label>
                     <input type="password" name="new_password" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500" required>
                 </div>
