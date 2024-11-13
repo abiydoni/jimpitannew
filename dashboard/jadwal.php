8c0a211e14a3b7cc0efb1678985fab9510f48bb3
abiydoni <abiydoni@gmail.com>
2024-11-14 00:40:44 +0700

    Update jadwal.php

--- dashboard/jadwal.php
+++ dashboard/jadwal.php
@@ -117,10 +117,10 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <div class="head">
                         <h3>Data User dan Jadwal Jaga</h3>
                         <div class="mb-4 text-center">
-                            <button id="openModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <button type="button" id="openModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                 <i class='bx bx-plus' style="font-size:24px"></i> <!-- Ikon untuk tambah data -->
                             </button>
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                 <a href="api/users_print.php" class="flex items-center">
                                     <i class='bx bx-printer' style="font-size:24px"></i> <!-- Ikon untuk print report -->
                                 </a>
