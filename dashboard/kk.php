6235f58d12dbc769600ed49a36f2608c1a5aeae6
abiydoni <abiydoni@gmail.com>
2024-11-14 00:38:23 +0700

    Update kk.php

--- dashboard/kk.php
+++ dashboard/kk.php
@@ -116,10 +116,10 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <h3>KK</h3>
                         <div class="mb-4 text-center">
                             <button type="button" id="addDataBtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-modal-toggle="addModal" onclick="toggleModal('addModal')">
-                                <i class='bx bx-plus' style="font-size:22px"></i> <!-- Ikon untuk tambah data -->
+                                <i class='bx bx-plus' style="font-size:24px"></i> <!-- Ikon untuk tambah data -->
                             </button>
                             <button type="button" id="printSelectedBtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                <i class='bx bxs-printer' style="font-size:24px"></i>
+                                <i class='bx bx-printer' style="font-size:24px"></i> <!-- Ikon untuk print report -->
                             </button>
                         </div>
                     </div>
