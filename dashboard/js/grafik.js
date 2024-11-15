cb30226524491b618bc0689b90720c02de1b4b8c
abiydoni <abiydoni@gmail.com>
2024-11-15 23:35:20 +0700

    Update grafik.js

--- dashboard/js/grafik.js
+++ dashboard/js/grafik.js
@@ -1,43 +1,54 @@
 // Mengambil data dari API
-fetch('api/get_saldo.php')
-    .then(response => response.json())
-    .then(data => {
-        // Konfigurasi grafik dengan data dari database
-        createChart(data);
-    });
+fetch("api/get_saldo.php")
+  .then((response) => response.json())
+  .then((data) => {
+    // Konfigurasi grafik dengan data dari database
+    createChart(data);
+  });
 
 function createChart(saldoData) {
-    const data = {
-        //labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 
-        //        'Juli', 'Agustus', 'September', 'Oktober', 'Nopember', 'Desember'],
-        datasets: [{
-            label: 'Analisa Pemasukan dan Pengeluaran per Bulan',
-            data: saldoData,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-        }]
-    };
-    // ... konfigurasi chart lainnya
-    const config = {
-        type: 'bar',  // Jenis grafik: 'bar', 'line', 'pie', dll.
-        data: data,
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            },
-            // Menambahkan pengaturan tinggi grafik
-            responsive: true,
-            maintainAspectRatio: false, // Mengizinkan perubahan rasio aspek
-        }
-    };
-    // Menentukan tinggi grafik
-    document.getElementById('myChart').style.height = '400px'; // Ubah tinggi sesuai kebutuhan
-    // Render grafik ke elemen canvas dengan id "myChart"
-    const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    );
+  const data = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "Mei",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Okt",
+      "Nov",
+      "Des",
+    ], // Mengubah nama bulan menjadi tiga huruf
+    datasets: [
+      {
+        label: "Analisa Pemasukan dan Pengeluaran per Bulan",
+        data: saldoData,
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+  // ... konfigurasi chart lainnya
+  const config = {
+    type: "bar", // Jenis grafik: 'bar', 'line', 'pie', dll.
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      // Menambahkan pengaturan tinggi grafik
+      responsive: true,
+      maintainAspectRatio: false, // Mengizinkan perubahan rasio aspek
+    },
+  };
+  // Menentukan tinggi grafik
+  document.getElementById("myChart").style.height = "400px"; // Ubah tinggi sesuai kebutuhan
+  // Render grafik ke elemen canvas dengan id "myChart"
+  const myChart = new Chart(document.getElementById("myChart"), config);
 }
