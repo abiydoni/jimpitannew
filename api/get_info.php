c530177ccf8336398ef75aff74dc1505a75e3f5f
abiydoni <abiydoni@gmail.com>
2024-11-24 17:54:03 +0700

    scan

--- api/get_info.php
+++ api/get_info.php
@@ -3,12 +3,12 @@ include 'db.php';
 
 try {
     // Query untuk total Scan
-    $sqlScan = "SELECT COALESCE(SUM(nominal), 0) AS total_scan FROM report WHERE jimpitan_date = curdate()";
+    $sqlScan = "SELECT COALESCE(SUM(nominal), 0) AS total_scan FROM report WHERE jimpitan_date = CURDATE()";
     $stmtScan = $pdo->query($sqlScan);
     $totalScan = $stmtScan->fetch(PDO::FETCH_ASSOC)["total_scan"];
     
     // Query untuk total Scan
-    $sqlData = "SELECT COALESCE(count(nominal), 0) AS total_data FROM report WHERE jimpitan_date = '2024-11-23'";
+    $sqlData = "SELECT COALESCE(count(nominal), 0) AS total_data FROM report WHERE jimpitan_date = CURDATE()";
     $stmtData = $pdo->query($sqlData);
     $totalData = $stmtData->fetch(PDO::FETCH_ASSOC)["total_data"];
     
--- index.php
+++ index.php
@@ -130,7 +130,9 @@ include 'api/get_info.php';
     });
   </script>
   <p style="color:grey; font-size: 14px; text-align: center;" id="tanggalHariIni"></p>
-  <h4 id="totalScan">Jumlah Scan: Rp. <?php echo $totalScan ?> dan <?php echo $totaldata ?> KK</h4>
+  <h4 id="totalScan">
+    Jumlah Scan: Rp. <?php echo number_format($totalScan, 0, ',', '.'); ?> dan <?php echo $totaldata; ?> KK
+  </h4>
 
   <div class="floating-button" style="margin-right : 70px;">
     <a href="dashboard/logout.php"><i class="bx bx-log-out-circle bx-tada bx-flip-horizontal" style="font-size:24px" ></i></a>
