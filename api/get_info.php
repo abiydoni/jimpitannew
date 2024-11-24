6af05c8dd151a3d60f986f59b10d15bb519b8e60
abiydoni <abiydoni@gmail.com>
2024-11-24 18:01:39 +0700

    scan

--- api/get_info.php
+++ api/get_info.php
@@ -3,12 +3,12 @@ include 'db.php';
 
 try {
     // Query untuk total Scan
-    $sqlScan = "SELECT COALESCE(SUM(nominal), 0) AS total_scan FROM report WHERE jimpitan_date = '2014-11-23'";
+    $sqlScan = "SELECT COALESCE(SUM(nominal), 0) AS total_scan FROM report WHERE jimpitan_date = '2024-11-23'";
     $stmtScan = $pdo->query($sqlScan);
     $totalScan = $stmtScan->fetch(PDO::FETCH_ASSOC)["total_scan"];
     
     // Query untuk total Scan
-    $sqlData = "SELECT COALESCE(count(nominal), 0) AS total_data FROM report WHERE jimpitan_date = '2014-11-23'";
+    $sqlData = "SELECT COALESCE(count(nominal), 0) AS total_data FROM report WHERE jimpitan_date = '2024-11-23'";
     $stmtData = $pdo->query($sqlData);
     $totalData = $stmtData->fetch(PDO::FETCH_ASSOC)["total_data"];
     
