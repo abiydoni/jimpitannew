e8ac3a23d1053a6834169b1e2ebf2e66d0ec4615
abiydoni <abiydoni@gmail.com>
2024-11-27 01:10:20 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,27 @@
-const CACHE_NAME = 'jimpitan-cache-v1.1';
+const CACHE_NAME = "jimpitan-cache-v1.2";
 const urlsToCache = [
-  '/',
-  'index.php',
-  'login.php',
-  'manifest.json',
-  'assets/audio/interface.wav',
-  'assets/image/block.gif'
+  "/",
+  "index.php",
+  "login.php",
+  "detail.php",
+  "manifest.json",
+  "assets/audio/interface.wav",
+  "assets/image/block.gif",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
