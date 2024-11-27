31c0db01c1ca60e6a52954347b7dfa49071df09d
abiydoni <abiydoni@gmail.com>
2024-11-28 02:00:15 +0700

    coba

--- login.php
+++ login.php
@@ -54,9 +54,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <!-- <link rel="manifest" href="manifest.json"> -->
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <script type="module" src="js/ionicons.esm.js"></script>
+    <script nomodule src="js/ionicons.js"></script>
+    <link rel="stylesheet" href="css/normalize.min.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;600;800&display=swap'>
     <link rel="stylesheet" href="/css/styles.css">
 </head>
