f5ee071329895200ba046ba6b628bd9f325c40cb
abiydoni <abiydoni@gmail.com>
2024-12-01 19:31:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ session_start();
 
 // Check if user is logged in
 if (!isset($_SESSION['user'])) {
-    echo json_encode(['success' => false, 'message' => 'Login kadaluarsa, silahkan login kembali!']);
     header('Location: login.php'); // Redirect to login page
     exit;
 }
