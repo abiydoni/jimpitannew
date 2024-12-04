3270756df1ee3d0274fbf675123eafabd223972e
abiydoni <abiydoni@gmail.com>
2024-12-04 11:27:20 +0700

    Update detail_scan.php

--- api/detail_scan.php
+++ api/detail_scan.php
@@ -39,10 +39,11 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
+
 <div class="flex flex-col min-h-screen max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg">
     <a style="font-weight: bold; font-size: 15px;">Data Scan Jimpitan</a>
     <a style="color: grey; font-size: 10px;">Hari Ini : <span id="tanggal"></span></a>
-    <div class="table-container flex-1 overflow-y-auto border rounded-md mb-4">
+    <div class="table-container flex-1 overflow-y-auto border rounded-md mb-4" style="font-size: 12px;">
         <table>
             <thead>
                 <tr>
