86f8c261324238fa6eda3cdcb2836ff3c3e3ea79
abiydoni <abiydoni@gmail.com>
2024-12-13 11:07:02 +0700

    Update rekor_scan.php

--- api/rekor_scan.php
+++ api/rekor_scan.php
@@ -55,7 +55,7 @@ include 'db.php';
         <div class="table-container flex-1 border rounded-md mb-4" style="font-size: 12px;">
             <?php
                 // Eksekusi query
-                $stmt = $pdo->prepare("SELECT collector, COUNT(*) AS jumlah_scan FROM report GROUP BY collector ORDER BY jumlah_scan DESC");
+                $stmt = $pdo->prepare("SELECT collector, COUNT(*) AS jumlah_scan FROM report GROUP BY kode_u ORDER BY jumlah_scan DESC");
                 $stmt->execute();
                 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -81,7 +81,7 @@ include 'db.php';
                     foreach ($results as $row) {
                         echo "<tr class='border-b hover:bg-gray-50' data-no='{$no}'>
                                 <td>{$no}</td>
-                                <td>{$row['collector']}</td>
+                                <td>{$row['nama_u']}</td>
                                 <td class='text-right'>" . number_format($row['jumlah_scan'], 0, ',', '.') . "</td>
                                 <td>
                                     <div class='chart-container'>
