4928a69191fe4892ac6046bf2bb7de024490b684
abiydoni <abiydoni@gmail.com>
2024-12-13 23:52:00 +0700

    Update detail_scan.php

--- api/detail_scan.php
+++ api/detail_scan.php
@@ -46,7 +46,7 @@ $total_nominal = array_sum(array_column($data, 'nominal'));
     </style>
 </head>
 <body class="bg-gray-100 font-poppins text-gray-800">
-    <div class="flex flex-col max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg">
+    <div class="flex flex-col max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg" style="max-width: 50vh;">
         <h1 class="text-xl font-bold text-gray-700 mb-2">Data Scan Jimpitan</h1>
         <p class="text-sm text-gray-500 mb-4">Hari <span id="tanggal"></span></p>
 
