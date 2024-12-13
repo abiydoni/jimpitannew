c9c0aef08f28386140806ce4ab69e8d4641b78fe
abiydoni <abiydoni@gmail.com>
2024-12-13 23:29:41 +0700

    Update detail_scan.php

--- api/detail_scan.php
+++ api/detail_scan.php
@@ -46,11 +46,11 @@ $total_nominal = array_sum(array_column($data, 'nominal'));
     </style>
 </head>
 <body class="bg-gray-100 font-poppins text-gray-800">
-    <div class="flex flex-col max-h-[10px] max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg">
+    <div class="flex flex-col max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg">
         <h1 class="text-xl font-bold text-gray-700 mb-2">Data Scan Jimpitan</h1>
         <p class="text-sm text-gray-500 mb-4">Hari <span id="tanggal"></span></p>
 
-        <div class="table-container flex-1 border rounded-md mb-4 overflow-y-auto" style="font-size: 12px;">
+        <div class="max-h-[10px] flex-1 border rounded-md mb-4 overflow-y-auto" style="font-size: 12px;">
         <?php if (count($data) > 0): ?>
                 <table class='min-w-full border-collapse text-sm text-gray-700'>
                     <thead>
