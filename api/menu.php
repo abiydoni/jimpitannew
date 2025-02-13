3059232e7b3e4fc33814ae1a9d16083f840219d1
abiydoni <abiydoni@gmail.com>
2025-02-13 21:48:25 +0700

    Update menu.php

--- api/menu.php
+++ api/menu.php
@@ -24,7 +24,10 @@ include 'db.php';
 </head>
 <body class="bg-gray-100 font-poppins text-gray-800">
     <div class="flex flex-col max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg" style="max-width: 60vh;">
-        <h2>Halaman ini masih dalam pengembangan...</h2>
+        <h1>Halaman ini masih dalam pengembangan...</h1>
+        <div class="flex flex-col mx-auto p-4 bg-white shadow-lg rounded-lg"">
+            <h2>Halaman ini masih dalam pengembangan...</h2>
+        </div>
         <!-- Tombol Bulat -->
         <button class="fixed bottom-4 right-4 w-12 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-full flex items-center justify-center shadow-lg transition-transform transform hover:scale-110"
                 onclick="window.location.href='detail_scan.php'" title="Kembali ke halaman detail sebelumnya">
