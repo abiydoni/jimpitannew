5f0bf7b2a309a8c603884ee17fc820dcc0043219
abiydoni <abiydoni@gmail.com>
2025-02-14 18:50:40 +0700

    Update jimpitan.php

--- api/jimpitan.php
+++ api/jimpitan.php
@@ -90,9 +90,9 @@ foreach ($results as $row) {
                         <tr class="bg-gray-100 border-b">
                             <th>No.</th>
                             <th>Nama Kepala Keluarga</th>
-                            <th>Target</th>
-                            <th>Diambil</th>
-                            <th>Hutang</th>
+                            <th class="text-left">Target</th>
+                            <th class="text-left">Diambil</th>
+                            <th class="text-left">Hutang</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -106,9 +106,9 @@ foreach ($results as $row) {
                             <tr class="border-b hover:bg-gray-50">
                                 <td><?= $no ?></td>
                                 <td><?= $row['kk_name'] ?></td>
-                                <td><?= number_format($target, 0, ',', '.') ?></td>
-                                <td><?= number_format($row['jumlah_nominal'], 0, ',', '.') ?></td>
-                                <td><?= number_format($hutang, 0, ',', '.') ?></td>
+                                <td class="text-left"><?= number_format($target, 0, ',', '.') ?></td>
+                                <td class="text-left"><?= number_format($row['jumlah_nominal'], 0, ',', '.') ?></td>
+                                <td class="text-left"><?= number_format($hutang, 0, ',', '.') ?></td>
                             </tr>
                         <?php
                         $no++;
