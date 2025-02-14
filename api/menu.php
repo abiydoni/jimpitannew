47bdddeaa4f74b51255881f3d5a6a1beef9fd18c
abiydoni <abiydoni@gmail.com>
2025-02-14 23:03:06 +0700

    Update menu.php

--- api/menu.php
+++ api/menu.php
@@ -36,9 +36,9 @@ try {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
 
-<body class="bg-gray-100 font-poppins text-gray-800 flex justify-center items-center min-h-screen">
+<body class="bg-gray-100 font-poppins text-gray-800">
     <div class="flex flex-col max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg" style="max-width: 60vh;">
-        <h1 class="text-2xl font-bold text-gray-700 mb-4 flex items-center">
+        <h1 class="text-2xl font-bold text-gray-700 mb-2 flex items-center">
             <!-- Ikon Ionicons -->
             <ion-icon name="information-circle-outline" class="text-3xl mr-2"></ion-icon>           
              Menu Informasi
