fc1b02923fd424e81d6d1c368e0b6271c94f08b7
abiydoni <abiydoni@gmail.com>
2025-02-14 22:48:09 +0700

    Update menu.php

--- api/menu.php
+++ api/menu.php
@@ -37,7 +37,7 @@ try {
 </head>
 
 <body class="bg-gray-100 font-poppins text-gray-800 flex justify-center items-center min-h-screen">
-    <div class="w-full max-w-lg p-6 bg-white shadow-lg rounded-lg">
+    <div class="flex flex-col max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg" style="max-width: 60vh;">
         <h1 class="text-2xl font-bold text-gray-700 mb-4 flex items-center">
             <!-- Ikon Ionicons -->
             <ion-icon name="information-circle-outline" class="text-3xl mr-2"></ion-icon>           
