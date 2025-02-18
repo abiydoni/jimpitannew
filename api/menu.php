13701bb4a742d38b5d58b7af9fc1c0d2dba33ac2
abiydoni <abiydoni@gmail.com>
2025-02-18 16:09:22 +0700

    Update menu.php

--- api/menu.php
+++ api/menu.php
@@ -83,7 +83,8 @@ try {
     </style>
 
 </head>
-<body class="bg-gray-100 font-poppins text-gray-800" style="background: url('<?= $background ?>') no-repeat center center fixed; background-size: cover;">>
+<body class="bg-gray-100 font-poppins text-gray-800"
+    style="background: url('<?= $background ?>') no-repeat center center fixed; background-size: cover;">
     <div class="flex flex-col max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg" style="max-width: 60vh;">
         <h1 class="text-2xl font-bold text-gray-700 mb-2 flex items-center">
             <ion-icon name="information-circle-outline" class="text-3xl mr-2"></ion-icon>           
