a80f5fd17f035ef89830b55df59ea5e827049c19
abiydoni <abiydoni@gmail.com>
2025-03-22 12:55:45 +0700

    js

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['user'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Jimpitan</title>
   
-  <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+  <script src="js/html5-qrcode.min/js" type="text/javascript"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
