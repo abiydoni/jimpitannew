b3f3ecdf831770a50112d8c258da2acb7807bbe1
abiydoni <abiydoni@gmail.com>
2025-04-30 13:37:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -108,13 +108,10 @@ try {
 
 </head>
 <body class="bg-gray-100 font-poppins text-gray-800">
-    <div class="absolute inset-0 bg-black bg-opacity-50"></div>
-
     <!-- Loader GIF loading -->
     <div id="loader" class="fixed inset-0 bg-white bg-opacity-80 flex items-center justify-center z-50 hidden">
         <img src="assets/image/loading.gif" alt="Loading..." class="w-32 h-auto">
     </div>
-
     <div class="relative z-10"> 
 
         <div class="flex flex-col max-w-4xl mx-auto p-4 rounded-lg" style="max-width: 60vh;">
