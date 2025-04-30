57fda598e36aa7233e1495abde2a1d0f34d277aa
abiydoni <abiydoni@gmail.com>
2025-04-30 14:59:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -118,11 +118,16 @@ try {
     <div class="relative z-10"> 
 
         <div class="flex flex-col max-w-4xl mx-auto p-4 rounded-lg" style="max-width: 60vh;">
-            <h2 class="text-2xl font-bold text-gray-700 mb-2 flex items-center">
-                <ion-icon name="information-circle-outline" class="text-3xl mr-2"></ion-icon>           
-                Menu Jimpitan
+            <h2 class="text-2xl font-bold text-gray-700 mb-2 flex items-center justify-between">
+                <div class="flex items-center">
+                    <ion-icon name="information-circle-outline" class="text-3xl mr-2"></ion-icon>
+                    Menu Jimpitan
+                </div>
+                <input type="color" id="overlayColor" 
+                    style="z-index:9999;" 
+                    class="w-8 h-8 rounded-full border-none p-0 cursor-pointer" 
+                    title="Pilih warna latar belakang">
             </h2>
-            <input type="color" id="overlayColor" style="z-index:9999; position:relative;" ...>
             <div class="flex flex-col items-center p-2 rounded-lg mb-2 bg-gray-800 opacity-50">
                 <div class="text-sm font-semibold text-white overflow-hidden w-full">
                     <span class="animate-marquee"><?= htmlspecialchars($profil['catatan']) ?></span>
