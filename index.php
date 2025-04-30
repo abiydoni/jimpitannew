e96d011560216928e76956225882da7a48aa200e
abiydoni <abiydoni@gmail.com>
2025-04-30 12:29:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -110,7 +110,11 @@ try {
 <body class="bg-gray-100 font-poppins text-gray-800"
     style="background: url('<?= $background ?>') no-repeat center center fixed; background-size: cover;">
     <div class="absolute inset-0 bg-black bg-opacity-50"></div>
-    
+    <!-- Loader fullscreen -->
+    <div id="loader" class="fixed inset-0 bg-white bg-opacity-80 flex items-center justify-center z-50 hidden">
+        <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-600 border-solid border-4 border-opacity-50"></div>
+    </div>
+
     <div class="relative z-10"> 
 
         <div class="flex flex-col max-w-4xl mx-auto p-4 rounded-lg" style="max-width: 60vh;">
@@ -167,6 +171,23 @@ try {
             50% { transform: translateY(-10px); }
         }
     </style>
+
+    <script>
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', function (e) {
+        // Hindari link dengan target _blank atau link anchor
+        if (
+            this.target !== '_blank' &&
+            this.href &&
+            !this.href.startsWith('javascript') &&
+            !this.href.startsWith('#')
+        ) {
+            // Tampilkan loader
+            document.getElementById('loader').classList.remove('hidden');
+        }
+        });
+    });
+    </script>
 
     <script>
         // Fungsi untuk mendapatkan tanggal dan waktu saat ini dalam format Indonesia
