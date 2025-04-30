3f03cffa7c8dc92ddf1340f9ab0518228f225f2f
abiydoni <abiydoni@gmail.com>
2025-04-30 14:10:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -115,7 +115,9 @@ try {
     <label for="overlayColor" class="text-white">Pilih Warna Latar:</label>
     <input type="color" id="overlayColor" name="overlayColor" value="#000000" class="w-10 h-10 rounded-full border-none p-0">
 
-    <div id="overlayDiv" class="absolute inset-0 shadow bg-black bg-opacity-90"></div>
+    <!-- <div id="overlayDiv" class="absolute inset-0 shadow bg-black bg-opacity-90"></div> -->
+    <div id="overlayDiv" class="absolute inset-0"></div>
+
     <div class="relative z-10"> 
 
         <div class="flex flex-col max-w-4xl mx-auto p-4 rounded-lg" style="max-width: 60vh;">
