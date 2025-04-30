e817a9a7b46308bb73c707ee2ee02b84fefdc1bd
abiydoni <abiydoni@gmail.com>
2025-04-30 14:54:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ try {
             </div>
 
             <div class="fixed bottom-4 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
-                <h1 class="text-center font-bold mb-2 text-white">Scan Disini..!</h1>
+                <h1 class="text-center font-bold mb-2 text-gray-500">Scan Disini..!</h1>
                 <a href="scan.php" 
                 class="w-20 h-20 bg-red-600 hover:bg-red-800 text-white rounded-full flex items-center justify-center shadow-lg transition-transform transform hover:scale-110">
                     <ion-icon name="barcode-outline" class="text-4xl"></ion-icon>
