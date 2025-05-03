c6d5452b969afcf0aa11fd55412c1ce55526d466
abiydoni <abiydoni@gmail.com>
2025-05-04 01:59:35 +0700

    Update pesan.php

--- api/pesan.php
+++ api/pesan.php
@@ -15,7 +15,7 @@ include 'db.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data KK</title>
+    <title>Kirim Pesan</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
@@ -43,7 +43,7 @@ include 'db.php';
             </div>
             <div>
                 <label for="message" class="block text-sm font-medium">Pesan</label>
-                <textarea id="message" name="message" required
+                <textarea id="message" name="message" required aria-valuetext="Test Pesan  Otomatis"
                         class="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                         rows="3" placeholder="Tulis pesan Anda di sini..."></textarea>
             </div>
