98ee3956631d4beead0572d8bb3073906ec02a93
abiydoni <abiydoni@gmail.com>
2025-05-07 00:06:24 +0700

    Update send-wa-group.php

--- api/send-wa-group.php
+++ api/send-wa-group.php
@@ -10,11 +10,8 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 // groupId = '6285729705810-1505093181@g.us' // == "Warga rt 07/01 randsa"
 // gorupId = '120363398680818900@g.us' // 'Group WA Q'
 // Ambil dan sanitasi input
-// $groupList = $_POST['groupId'] ?? [];
-// $pesangroup = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
-$input = json_decode(file_get_contents('php://input'), true);
-$groupList = $input['groupId'] ?? [];
-$pesangroup = $input['message'] ?? '';
+$groupList = $_POST['groupId'] ?? [];
+$pesangroup = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
 
 // Validasi
 if (empty($groupList) || !$pesangroup) {
@@ -29,9 +26,10 @@ $successCount = 0;
 $errorCount = 0;
 
 foreach ($groupList as $group) {
-    $group = trim($group);
-    if (empty($group) || !str_ends_with($group, '@g.us')) continue;
-    
+    $group = filter_var($group, FILTER_SANITIZE_NUMBER_INT);
+
+    if (!$group) continue;
+
     $data = [
         'groupId' => $group,
         'message' => $pesangroup
