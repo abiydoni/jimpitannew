eaf07ae278d03a9b5a974ce57ac518e6271adea2
abiydoni <abiydoni@gmail.com>
2025-05-09 16:57:31 +0700

    Update inventaris.php

--- dashboard/inventaris.php
+++ dashboard/inventaris.php
@@ -19,11 +19,9 @@
         <div class="order">
             <div class="head">
                 <h3>Inventaris RT</h3>
-                <div class="mb-4 text-center">
-                    <button type="button" id="openModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        <i class='bx bx-plus' style="font-size:24px"></i> <!-- Ikon untuk tambah data -->
-                    </button>
-                </div>
+
+
+                
             </div>
         </div>
     </div>
