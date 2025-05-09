83aee23c64a0ea470799504c4196eaa19aabaffe
abiydoni <abiydoni@gmail.com>
2025-05-09 17:05:03 +0700

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,20 +1,6 @@
+<?php include 'header.php'; ?>
+
 <?php
-session_start();
-
-// Check if user is logged in
-if (!isset($_SESSION['user'])) {
-    header('Location: ../login.php'); // Redirect to login page
-    exit;
-}
-
-// Check if user is admin
-if ($_SESSION['user']['role'] !== 'admin') {
-    header('Location: ../login.php'); // Redirect to unauthorized page
-    exit;
-}
-// Include the database connection
-include 'api/db.php';
-
 // Get the current day of the week (e.g., "Sunday", "Monday", etc.)
 $currentDay = date('l'); // 'l' gives full textual representation of the day
 
@@ -36,61 +22,6 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 include 'api/get_info.php';
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Boxicons -->
-    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-    <!-- My CSS -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <title>Jimpitan - RT07 Salatiga</title>
-</head>
-<body>
-    <!-- SIDEBAR -->
-    <section id="sidebar">
-        <a href="#" class="brand">
-            <i class='bx bx-square-rounded'></i>
-            <span class="text">Jimpitan</span>
-        </a>
-        <ul class="side-menu top">
-            <li class="active"><a href="#"><i class='bx bxs-dashboard'></i><span class="text">Dashboard</span></a></li>
-            <li><a href="jadwal.php"><i class='bx bxs-group'></i><span class="text">Jadwal Jaga</span></a></li>
-            <li><a href="kk.php"><i class='bx bxs-group'></i><span class="text">KK</span></a></li>
-            <li><a href="report.php"><i class='bx bxs-report'></i><span class="text">Report</span></a></li>
-            <li><a href="keuangan.php"><i class='bx bxs-wallet'></i><span class="text">Keuangan</span></a></li>
-        </ul>
-        <ul class="side-menu">
-            <li><a href="setting.php"><i class='bx bxs-cog'></i><span class="text">Settings</span></a></li>
-            <li><a href="logout.php" class="logout"><i class='bx bxs-log-out-circle'></i><span class="text">Logout</span></a></li>
-        </ul>
-    </section>
-    <!-- SIDEBAR -->
-
-    <!-- CONTENT -->
-    <section id="content">
-        <!-- NAVBAR -->
-        <nav>
-            <i class='bx bx-menu' ></i>
-            <form action="#">
-                <div class="form-input">
-                    <!-- <input type="search" id="search-input" placeholder="Search...">
-                    <button type="submit" class="search-btn"><i class='bx bx-search' ></i></button> -->
-                </div>
-            </form>
-            <input type="checkbox" id="switch-mode" hidden>
-            <label for="switch-mode" class="switch-mode"></label>
-        </nav>
-        <!-- NAVBAR -->
 
         <!-- MAIN -->
         <main>
@@ -214,13 +145,5 @@ include 'api/get_info.php';
                             </div> -->
                     </main>
             <!-- MAIN -->
-        </section>
-    
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script src="js/script.js"></script>
-    <script src="js/grafik.js"></script>
-
-</body>
-</html>
+<?php include 'footer.php'; ?>
