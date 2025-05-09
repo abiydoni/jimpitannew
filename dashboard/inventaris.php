02cbddc37765d83fe361c858dd0d3cb3ec9849a7
abiydoni <abiydoni@gmail.com>
2025-05-09 16:55:18 +0700

    aaaa

--- dashboard/inventaris.php
+++ dashboard/inventaris.php
@@ -1,3 +1,32 @@
 <?php include 'header.php'; ?>
+<!-- MAIN -->
+<main>
+    <div class="head-title">
+        <div class="left">
+            <h1>Jimpitan - RT07 Salatiga</h1>
+            <ul class="breadcrumb">
+                <li>
+                    <a href="#">Inventaris</a>
+                </li>
+                <li><i class='bx bx-chevron-right' ></i></li>
+                <li>
+                    <a class="active" href="index.php">Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="table-data">
+        <div class="order">
+            <div class="head">
+                <h3>Inventaris RT</h3>
+                <div class="mb-4 text-center">
+                    <button type="button" id="openModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        <i class='bx bx-plus' style="font-size:24px"></i> <!-- Ikon untuk tambah data -->
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php include 'footer.php'; ?>
