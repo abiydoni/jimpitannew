3b89cb5bb8157fa3fd24a970de56e9964bc89237
abiydoni <abiydoni@gmail.com>
2025-05-11 23:17:50 +0700

    Update ambil_data_jimpitan.php

--- api/ambil_data_jimpitan.php
+++ api/ambil_data_jimpitan.php
@@ -47,7 +47,7 @@ $stmt = $pdo->prepare("
     ORDER BY report.scan_time DESC
 ");
 $stmt->execute();
-$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$data = $stmt->fetchAll();
 
 // Hitung total nominal
 $total_nominal = array_sum(array_column($data, 'nominal'));
