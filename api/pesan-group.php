4fbbfb0bbd71c33df04916cb6b1b7aa532953ede
abiydoni <abiydoni@gmail.com>
2025-05-13 14:27:21 +0700

    Update pesan-group.php

--- api/pesan-group.php
+++ api/pesan-group.php
@@ -65,7 +65,7 @@ $status = isset($_GET['status']) ? $_GET['status'] : '';
             <?php include 'ambil_data_jimpitan.php'; ?>
             <div class="input-group mt-4">
                 <label>Pesan:</label><br>
-                <textarea name="message" rows="15" cols="50" placeholder="Tulis pesan..." class="w-full px-2 py-2 border rounded"><?php echo htmlspecialchars($pesan); ?></textarea>
+                <textarea name="message" rows="17" cols="50" placeholder="Tulis pesan..." class="w-full px-2 py-2 border rounded"><?php echo htmlspecialchars($pesan); ?></textarea>
             </div>
 
             <br>
