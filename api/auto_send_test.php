1259babf78767d28d43d8cf47e02fe5762e5b026
abiydoni <abiydoni@gmail.com>
2025-05-16 08:37:34 +0700

    Create auto_send_test.php

---
+++ api/auto_send_test.php
@@ -0,0 +1,24 @@
+<?php
+include 'db.php';
+include 'ambil_data_jimpitan.php';
+$gorupId = '120363398680818900@g.us'; // 'Group WA Q'
+$message = $pesan;
+
+$data = http_build_query([
+    'groupId[]' => $groupId,
+    'message' => $message
+]);
+
+$ch = curl_init("https://rt07.appsbee.my.id/api/send-wa-group.php");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/x-www-form-urlencoded'
+]);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+?>
