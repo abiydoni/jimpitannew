fd2fdb896bb2996cf80882939f6fdfa4fe9cf06f
abiydoni <abiydoni@gmail.com>
2025-05-19 17:08:45 +0700

    Update send_wa_group_test.php

--- api/send_wa_group_test.php
+++ api/send_wa_group_test.php
@@ -16,7 +16,7 @@ if (empty($groupList) || !$pesangroup) {
     exit;
 }
 
-$url = "http://wapi.appsbee.my.id/send-group-message";
+$url = "https://wapi.appsbee.my.id/send-group-message";
 
 $logAll = "";
 $successCount = 0;
