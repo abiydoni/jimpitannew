abe4ad622603b8eb1ea0ccd7f247d4c7384f7b0f
abiydoni <abiydoni@gmail.com>
2025-05-23 18:45:01 +0700

    Update pesan_group.php

--- api/pesan_group.php
+++ api/pesan_group.php
@@ -34,6 +34,16 @@ $groupId = "6285729705810-1505093181@g.us";
         }
     </style>
     <script src="https://cdn.jsdelivr.net/npm/@joeattardi/emoji-button@4.6.2/dist/index.min.js"></script>
+    <style>
+    .emoji-picker {
+    z-index: 999999 !important;
+    position: absolute !important;
+    }
+    body, html, .container {
+    overflow: visible !important;
+    }
+    </style>
+
 </head>
 <body class="bg-gray-100 font-poppins text-gray-800">
     <div class="flex flex-col max-w-4xl mx-auto p-4 bg-white shadow-lg rounded-lg" style="max-width: 60vh;">
@@ -159,6 +169,12 @@ $groupId = "6285729705810-1505093181@g.us";
   button.addEventListener('click', () => {
     picker.togglePicker(button);
   });
+
+  const picker = new EmojiButton({
+  position: 'top-start', // atau 'bottom-start' kalau terlalu atas
+  rootElement: document.body
+  });
+
 </script>
 
 </body>
