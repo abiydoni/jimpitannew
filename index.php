0e949702a31eb7885267b333e74943e1fe322d49
abiydoni <abiydoni@gmail.com>
2025-05-24 00:12:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (!$user) {
 }
 
 $menus = $_SESSION['menus'] ?? [];
-include 'db.php';
+require_once 'api/db.php';
 $stmt = $conn->query("SELECT catatan FROM tb_profil LIMIT 1");
 $profil = $stmt->fetch(PDO::FETCH_ASSOC);
 $catatan = $profil['catatan'] ?? '';
