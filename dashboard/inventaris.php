c6995521642e0a04751005fe2a19c591f0448d3a
abiydoni <abiydoni@gmail.com>
2025-06-04 22:53:06 +0700

    Update inventaris.php

--- dashboard/inventaris.php
+++ dashboard/inventaris.php
@@ -1,20 +1,5 @@
 <?php include 'header.php'; ?>
 <!-- MAIN -->
-<main>
-    <div class="head-title">
-        <div class="left">
-            <h1>Jimpitan - RT07 Salatiga</h1>
-            <ul class="breadcrumb">
-                <li>
-                    <a href="#">Inventaris</a>
-                </li>
-                <li><i class='bx bx-chevron-right' ></i></li>
-                <li>
-                    <a class="active" href="index.php">Home</a>
-                </li>
-            </ul>
-        </div>
-    </div>
     <div class="table-data">
         <div class="order">
             <div class="head">
@@ -25,6 +10,4 @@
             </div>
         </div>
     </div>
-</main>
-
 <?php include 'footer.php'; ?>
