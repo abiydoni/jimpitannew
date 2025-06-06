3e38faf8a3d4dfdbf8607bd40f95064cfd0188ec
abiydoni <abiydoni@gmail.com>
2025-06-06 16:48:35 +0700

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -70,12 +70,6 @@ $menuItems = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <!-- NAVBAR -->
         <nav>
             <i class='bx bx-menu' ></i>
-            <form action="#">
-                <div class="form-input">
-                    <input type="search" id="search-input" placeholder="Search...">
-                    <button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
-                </div>
-            </form>
             <input type="checkbox" id="switch-mode" hidden>
             <label for="switch-mode" class="switch-mode"></label>
         </nav>
