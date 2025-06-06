63eba0b4d8689222c6290a10fb54433f81585fcb
abiydoni <abiydoni@gmail.com>
2025-06-06 09:26:36 +0700

    Update detail_scan.php

--- api/detail_scan.php
+++ api/detail_scan.php
@@ -165,7 +165,7 @@ $total_nominal = array_sum(array_column($data, 'nominal'));
                                 <td>${row.kk_name}</td>
                                 <td class="text-center">${parseInt(row.nominal).toLocaleString()}</td>
                                 <td>${collectorText}</td>
-                                <td><?= htmlspecialchars(substr($row["scan_time"], 11)) ?></td>
+                                <td>${row.scan_time.substring(11)}</td>
                             </tr>
                         `);
                     });
