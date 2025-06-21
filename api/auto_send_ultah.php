5e7c1e6ea9f188555e34c2d496f4b8ecae40ce91
abiydoni <abiydoni@gmail.com>
2025-06-21 23:54:34 +0700

    ultah

---
+++ api/auto_send_ultah.php
@@ -0,0 +1,35 @@
+<?php
+include 'get_konfigurasi.php';
+
+// Ambil konfigurasi dari database
+$filePesan = get_konfigurasi('report3');
+$groupId = get_konfigurasi('group_id2');
+$apiUrl = get_konfigurasi('api_url_group');
+$sessionId = get_konfigurasi('session_id');
+
+// Ambil isi pesan dari file konfigurasi
+include $filePesan;
+$message = $pesan;
+
+$data = http_build_query([
+    'groupId[]' => $groupId,
+    'message' => $message
+]);
+
+$ch = curl_init($apiUrl);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/x-www-form-urlencoded',
+    'x-session-id: ' . $sessionId
+]);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+// Opsional: Tampilkan respons
+// echo $response;
+?>
