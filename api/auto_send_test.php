4435a241df35ac2ab2f5f53693cd86a5a481cd53
abiydoni <abiydoni@gmail.com>
2025-06-26 00:05:57 +0700

    Update auto_send_test.php

--- api/auto_send_test.php
+++ api/auto_send_test.php
@@ -2,7 +2,7 @@
 include 'get_konfigurasi.php';
 
 // Ambil konfigurasi dari database
-$filePesan = get_konfigurasi('report1');
+$filePesan = get_konfigurasi('report3');
 $groupId = get_konfigurasi('group_id2');
 $apiUrl = get_konfigurasi('api_url_group');
 $sessionId = get_konfigurasi('session_id');
