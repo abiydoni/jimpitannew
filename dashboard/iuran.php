36621322bca130966ba36a8fc8c712354e6baaea
abiydoni <abiydoni@gmail.com>
2025-06-26 17:49:42 +0700

    Update iuran.php

--- dashboard/iuran.php
+++ dashboard/iuran.php
@@ -99,15 +99,9 @@ if (isset($_GET['detail_nikk'], $_GET['detail_tahun'])) {
       <div class="mb-4 relative">
         <label class="block mb-1">No KK / Nama KK</label>
         <input x-model="search" @focus="open = true" @input="open = true" type="text" placeholder="Cari No KK atau Nama KK..." class="w-full border rounded p-2" autocomplete="off" required>
-        <ul x-show="open" @click.away="open = false" class="absolute bg-white border w-full mt-1 rounded max-h-48 overflow-auto z-10">
-          <template x-if="filteredOptions.length > 0">
-            <template x-for="(kk, idx) in filteredOptions" :key="kk.nikk ?? idx">
-              <li @click="selectOption(kk)" class="px-2 py-1 hover:bg-blue-500 hover:text-white cursor-pointer"
-                  x-text="(kk.nikk ? kk.nikk : '-') + ' - ' + (kk.kk_name ? kk.kk_name : '-')"></li>
-            </template>
-          </template>
-          <template x-if="filteredOptions.length === 0">
-            <li class="px-2 py-1 text-gray-400">Data tidak ditemukan</li>
+        <ul x-show="open && filteredOptions.length > 0" @click.away="open = false" class="absolute bg-white border w-full mt-1 rounded max-h-48 overflow-auto z-10">
+          <template x-for="kk in filteredOptions" :key="kk.nikk">
+            <li @click="selectOption(kk)" class="px-2 py-1 hover:bg-blue-500 hover:text-white cursor-pointer" x-text="kk.nikk + ' - ' + kk.kk_name"></li>
           </template>
         </ul>
         <input type="hidden" name="nikk" :value="selectedOption ? selectedOption.nikk : ''" required>
@@ -218,19 +212,19 @@ function kkDropdownSearch() {
             if (!this.search) return this.options;
             const term = this.search.toLowerCase();
             return this.options.filter(kk =>
-                (kk.nikk && kk.nikk.toLowerCase().includes(term)) ||
-                (kk.kk_name && kk.kk_name.toLowerCase().includes(term))
+                kk.nikk.toLowerCase().includes(term) ||
+                kk.kk_name.toLowerCase().includes(term)
             );
         },
         selectOption(kk) {
             this.selectedOption = kk;
-            this.search = (kk.nikk ? kk.nikk : '-') + ' - ' + (kk.kk_name ? kk.kk_name : '-');
+            this.search = kk.nikk + ' - ' + kk.kk_name;
             this.open = false;
         },
         async init() {
             const res = await fetch('api/get_nikk_group.php');
             this.options = await res.json();
-            console.log('Data KK:', this.options);
+            console.log('NIKK options loaded:', this.options);
         }
     }
 }
