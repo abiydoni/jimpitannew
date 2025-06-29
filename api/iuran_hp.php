0f4cb2f45b76326b9e1160bb57f06f426bb268e3
abiydoni <abiydoni@gmail.com>
2025-06-29 22:19:28 +0700

    Update iuran_hp.php

--- api/iuran_hp.php
+++ api/iuran_hp.php
@@ -2,11 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// Pastikan pengguna sudah login
-if (!isset($_SESSION['user'])) {
-  header('Location: ../login.php'); // Redirect ke halaman login
-  exit;
-}
 
 include 'db.php';
 
