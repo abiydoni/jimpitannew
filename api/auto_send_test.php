1a482eb0fb194b758c4f4424f883d18def049c6b
abiydoni <abiydoni@gmail.com>
2025-11-30 00:56:14 +0700

    Update auto_send_test.php

--- api/auto_send_test.php
+++ api/auto_send_test.php
@@ -32,7 +32,8 @@ $telegramApiBase = rtrim((string)$gatewayBase, '/');
 $url = "{$telegramApiBase}/bot{$token}/sendMessage";
 $data = [
     'chat_id' => $chatId,
-    'text' => $message
+    'text' => $message,
+    'parse_mode' => 'Markdown'
 ];
 
 $ch = curl_init($url);
@@ -46,6 +47,32 @@ $result = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 $curlError = curl_error($ch);
 curl_close($ch);
+
+// Jika Markdown parse error, coba tanpa parse_mode
+if ($httpCode === 400 && $result) {
+    $error = json_decode($result, true);
+    if (isset($error['description']) && 
+        (stripos($error['description'], 'parse') !== false || 
+         stripos($error['description'], 'markdown') !== false)) {
+        // Coba kirim lagi tanpa parse_mode
+        $dataNoMarkdown = [
+            'chat_id' => $chatId,
+            'text' => $message
+        ];
+        
+        $ch2 = curl_init($url);
+        curl_setopt($ch2, CURLOPT_POST, true);
+        curl_setopt($ch2, CURLOPT_POSTFIELDS, json_encode($dataNoMarkdown));
+        curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch2, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch2, CURLOPT_TIMEOUT, 30);
+        
+        $result = curl_exec($ch2);
+        $httpCode = curl_getinfo($ch2, CURLINFO_HTTP_CODE);
+        $curlError = curl_error($ch2);
+        curl_close($ch2);
+    }
+}
 
 // Log error jika gagal
 if ($httpCode != 200) {
