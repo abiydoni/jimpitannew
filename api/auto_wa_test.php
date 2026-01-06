15d475b8ed3107a53a3a2f3aeb9d894140f7868c
abiydoni <abiydoni@gmail.com>
2026-01-06 12:55:16 +0700

    Update auto_wa_test.php

--- api/auto_wa_test.php
+++ api/auto_wa_test.php
@@ -1,7 +1,7 @@
 <?php
 // Konfigurasi - GANTI dengan nilai Anda
 $groupId = '120363123456789012@g.us'; // Group ID WhatsApp
-$gatewayBase = 'http://localhost:8000'; // URL wagateway (tanpa endpoint)
+$gatewayBase = 'http://127.0.0.1:8000'; // URL wagateway (gunakan 127.0.0.1 jika localhost tidak work)
 $message = 'Test pesan - ' . date('Y-m-d H:i:s');
 
 // Langsung kirim pesan (endpoint akan return error jika belum ready)
@@ -20,6 +20,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4); // Force IPv4
 
 $result = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
